2032ee86f4aa54ab476028306c1bf8d11f4fb986
Przemek Rękawiecki <przemek.rek@gmail.com>
2014-07-12 22:38:34 +0200

    controlling arduino according to the new specification

--- rover.ino
+++ rover.ino
@@ -1,24 +1,40 @@
 #define SERIAL_FIELDS_COUNT 3
-#define SERVO_MAX_DELTA 150
 
 #define SERIAL_COMMAND_FIELD 0
 #define SERIAL_VALUE_FIELD 1
 #define SERIAL_REQUEST_ID 2
 
+#define STATUS_OK 1
+
+#define E_SERIAL_INVALID_REQUEST -1
+#define E_SERIAL_IDLE -2
+#define E_SERIAL_INVALID_COMMAND -3
+#define E_VOLTAGE_MEASURE_ERROR -4 
+
 #define SC_HEALTHCHECK 0
 #define SC_SERVO_POSITION 1
 #define SC_MOTOR_SPEED 2
 
-#define E_SERIAL_INVALID_REQUEST = -1
-#define E_SERVO_WRONG_POSITION = -10
+
+#define SERVO_VALUE_MIN 0
+#define SERVO_VALUE_MAX 1024
+#define SERVO_TARGET_OFFSET 10
+#define SERVO_SLOW_SPEED 50
+#define SERVO_MAX_DELTA 150
+
+#define E_SERVO_WRONG_POSITION -10
+
+#define MOTOR_VALUE_MIN 0
+#define MOTOR_VALUE_NEUTRAL 50
+#define MOTOR_VALUE_MAX 100
+
+#define E_MOTOR_INVALID_SPEED -20
+
+#define MAX_IDLE_TIME 1000
 
 
-int SERVO_VALUE_MIN = 0;
-int SERVO_VALUE_MAX = 1024;
+int requestID = 0;
 
-int MOTOR_VALUE_MIN = 0;
-int MOTOR_VALUE_NEUTRAL = 10;
-int MOTOR_VALUE_MAX = 20;
 
 int fieldIndex = 0;               // the current field being received
 int serial_values[SERIAL_FIELDS_COUNT];  // array holding values for all the fields
@@ -41,97 +57,87 @@ int motorIn2A = 7;
 
 int voltageInput = A2;
 double voltageValue = 0.0;
-/* double VOLTAGE_CONST = 0.01563; // for 22KOhm/10KOhm */
-double VOLTAGE_CONST = 0.011842; // for 10KOhm/6.8KOhm
+double VOLTAGE_CONST = 0.01563; // for 22KOhm/10KOhm
+/* double VOLTAGE_CONST = 0.011842; // for 10KOhm/6.8KOhm */
 
+long lastActionTime;
 
 int setMotorSpeed(int speedTarget)
 {
-  int NEUTRAL = MOTOR_VALUE_NEUTRAL;
-  //MOTOR
-  if(speedTarget < NEUTRAL)
-  {
-    //reverse
-    speedTarget = (-speedTarget + NEUTRAL) * (255 /  NEUTRAL) + 5;
-    Serial.print("new reverse speed target ready: ");
-    Serial.println(speedTarget);
-    
-    digitalWrite(motorIn1A, HIGH);
-    digitalWrite(motorIn2A, LOW);
-    /* digitalWrite(motorEnablePin, HIGH); */
-    analogWrite(motorEnablePin, speedTarget);
-  }
-  else if(speedTarget > NEUTRAL)
-  {
-    //forward
-    speedTarget = (speedTarget - NEUTRAL) * (255 /  NEUTRAL) + 5;
-    Serial.print("new forward speed target ready: ");
-    Serial.println(speedTarget);
-    
-    digitalWrite(motorIn1A, LOW);
-    digitalWrite(motorIn2A, HIGH);
-    /* digitalWrite(motorEnablePin, HIGH); */
-    analogWrite(motorEnablePin, speedTarget);
-  }
-  else digitalWrite(motorEnablePin, LOW);
- 
-  return 1; 
+    int NEUTRAL = MOTOR_VALUE_NEUTRAL;
+
+    if(speedTarget < MOTOR_VALUE_MIN || speedTarget > MOTOR_VALUE_MAX)
+    {
+        return E_MOTOR_INVALID_SPEED;
+    }
+
+    if(speedTarget < NEUTRAL)
+    {
+        //reverse
+        speedTarget = (-speedTarget + NEUTRAL) * (255 /  NEUTRAL) + 5;
+
+        digitalWrite(motorIn1A, HIGH);
+        digitalWrite(motorIn2A, LOW);
+        analogWrite(motorEnablePin, speedTarget);
+    }
+    else if(speedTarget > NEUTRAL)
+    {
+        //forward
+        speedTarget = (speedTarget - NEUTRAL) * (255 /  NEUTRAL) + 5;
+
+        digitalWrite(motorIn1A, LOW);
+        digitalWrite(motorIn2A, HIGH);
+        analogWrite(motorEnablePin, speedTarget);
+    }
+    else
+    {
+        digitalWrite(motorEnablePin, LOW);
+    }
+
+    return speedTarget;
 }
 
 int setServoPossition(int servoTarget)
 {
-    int status = 1;
+    if(servoTarget < SERVO_VALUE_MIN || servoTarget > SERVO_VALUE_MAX)
+    {
+        return E_SERVO_WRONG_POSITION;
+    }
 
     //setting new servo target
-    if(servoTarget >= SERVO_VALUE_MIN and servoTarget <= SERVO_VALUE_MAX)
-    {
-        servoPosTarget = servoTarget;
-    }
-    else
-    {
-        // invalid servo target value
-        status = -1;
-    }
+    servoPosTarget = servoTarget;
 
-    return status; 
+    return STATUS_OK;
 }
 
 int possitionServo()
 {
-    //SERVO
     servoPosValue = analogRead(servoPosPin);
 
-    if(servoPosTarget < servoPosValue - 10)
+    if(servoPosTarget < servoPosValue - SERVO_TARGET_OFFSET)
     {
-
-        /* Serial.print("pos too high: "); */
-        /* Serial.println(servoPosValue); */
-
         digitalWrite(servoIn1A, HIGH);
         digitalWrite(servoIn2A, LOW);
 
         servoDelta = servoPosValue - servoPosTarget;
         if(servoDelta < SERVO_MAX_DELTA)
         {
-            analogWrite(servoEnablePin, 50);
+            analogWrite(servoEnablePin, SERVO_SLOW_SPEED);
         }
         else
         {
             digitalWrite(servoEnablePin, HIGH);
         }
     }
-    else if(servoPosTarget > servoPosValue + 10)
+    else if(servoPosTarget > servoPosValue + SERVO_TARGET_OFFSET)
     {
-        /* Serial.print("pos too low: "); */
-        /* Serial.println(servoPosValue); */
-
         digitalWrite(servoIn1A, LOW);
         digitalWrite(servoIn2A, HIGH);
 
         servoDelta =  servoPosTarget - servoPosValue;
         if(servoDelta < SERVO_MAX_DELTA)
         {
-            analogWrite(servoEnablePin, 50);
+            analogWrite(servoEnablePin, SERVO_SLOW_SPEED);
         }
         else
         {
@@ -142,6 +148,8 @@ int possitionServo()
     {
         digitalWrite(servoEnablePin, LOW);
     }
+    
+    return STATUS_OK;
 }
 
 
@@ -158,94 +166,148 @@ double measureVoltage()
     return v_sum / 10;
 }
 
+int resetSerial()
+{
+    for(int i=0; i < min(SERIAL_FIELDS_COUNT, fieldIndex+1); i++)
+    {
+        serial_values[i] = 0; // set the values to zero, ready for the next message
+    }
+    fieldIndex = 0;  // ready to start over
 
-void setup(){
-  Serial.begin(9600);
+    return STATUS_OK;
+}
 
-  pinMode(servoEnablePin, OUTPUT);
-  pinMode(servoIn1A, OUTPUT);
-  pinMode(servoIn2A, OUTPUT);
-  digitalWrite(servoEnablePin, LOW);
-  
-  pinMode(motorEnablePin, OUTPUT);
-  pinMode(motorIn1A, OUTPUT);
-  pinMode(motorIn2A, OUTPUT);
-  digitalWrite(motorEnablePin, LOW);
-  
-  setMotorSpeed(MOTOR_VALUE_NEUTRAL);
-  
-  servoPosTarget = SERVO_VALUE_MAX / 2;
+int sendSerialResponse(int requestID, int status, double value)
+{
+    Serial.print(requestID);
+    Serial.print(':');
+    Serial.print(status);
+    Serial.print(':');
+    Serial.println(value);
+}
+
+int processSerial()
+{
+    int status = STATUS_OK;
+    double value = 0.0;
+
+    if( Serial.available())
+    {
+        char ch = Serial.read();
+
+        if(ch >= '0' && ch <= '9') // is this an ascii digit between 0 and 9?
+        {
+            serial_values[fieldIndex] = (serial_values[fieldIndex] * 10) + (ch - '0'); 
+        }
+        else if (ch == ':')  // comma is our separator, so move on to the next field
+        {
+            fieldIndex++;   // increment field index 
+        }
+        else if (ch == '\r')
+        {
+            if(fieldIndex + 1 != SERIAL_FIELDS_COUNT || serial_values[SERIAL_REQUEST_ID] == 0)
+            {
+                status = E_SERIAL_INVALID_REQUEST;
+                sendSerialResponse(-1, status, value);
+                resetSerial();
+
+                return status;
+            }
+
+            requestID = serial_values[SERIAL_REQUEST_ID];
+
+            lastActionTime = millis();
+
+            switch(serial_values[SERIAL_COMMAND_FIELD])
+            {
+                case SC_HEALTHCHECK:
+                    value = measureVoltage();
+                    if(value < 0)
+                    {
+                        value = 0;
+                        status = E_VOLTAGE_MEASURE_ERROR;
+                    }
+                    break;
+
+                case SC_SERVO_POSITION:
+                    status = setServoPossition(serial_values[SERIAL_VALUE_FIELD]);
+                    break;
+
+                case SC_MOTOR_SPEED:
+                    setMotorSpeed(serial_values[SERIAL_VALUE_FIELD]);
+                    break;
+
+                default:
+                    //request error: invalid command
+                    status = E_SERIAL_INVALID_COMMAND;
+            }
+
+            //send response
+            if(status == STATUS_OK)
+            {
+                sendSerialResponse(requestID, status, value);
+            }
+            else
+            {
+                sendSerialResponse(-1, status, value);
+            }
+
+            resetSerial();
+        }
+        else if (ch == '\n')
+        {
+            //do noting
+        }
+        else
+        {
+            status = E_SERIAL_INVALID_REQUEST;
+            sendSerialResponse(-1, status, value);
+            resetSerial();
+            return status;
+        }
+    }
+
+    return status;
+}
+
+void setup()
+{
+    Serial.begin(9600);
+
+    pinMode(servoEnablePin, OUTPUT);
+    pinMode(servoIn1A, OUTPUT);
+    pinMode(servoIn2A, OUTPUT);
+    digitalWrite(servoEnablePin, LOW);
+
+    pinMode(motorEnablePin, OUTPUT);
+    pinMode(motorIn1A, OUTPUT);
+    pinMode(motorIn2A, OUTPUT);
+    digitalWrite(motorEnablePin, LOW);
+
+    setMotorSpeed(MOTOR_VALUE_NEUTRAL);
+
+    servoPosTarget = SERVO_VALUE_MAX / 2;
+
+    lastActionTime = millis();
 }
 
 void loop()
 {
-  if( Serial.available())
-  {
-    char ch = Serial.read();
+    int status = STATUS_OK;
 
-    if(ch >= '0' && ch <= '9') // is this an ascii digit between 0 and 9?
+    if(lastActionTime < (millis() - MAX_IDLE_TIME))
     {
-      if(fieldIndex < SERIAL_FIELDS_COUNT) {
-        serial_values[fieldIndex] = (serial_values[fieldIndex] * 10) + (ch - '0'); 
-      }
+        //stop all motors
+        setMotorSpeed(MOTOR_VALUE_NEUTRAL);
+        lastActionTime = millis();
+
+        //send error communicate
+        sendSerialResponse(-1, E_SERIAL_IDLE, 0);
     }
-    else if (ch == ':')  // comma is our separator, so move on to the next field
-    {
-      fieldIndex++;   // increment field index 
-    }
-    else if (ch == '\r')
-    {
-        //if fildIndex < SERIAL_FIELDS_COUNT error invalid request
+    //precaution against going back to zero the millis() result
+    if(lastActionTime > millis()) lastActionTime = millis();
 
-        switch(serial_values[SERIAL_COMMAND_FIELD])
-        {
-            case SC_HEALTHCHECK:
-                break;
-            case SC_SERVO_POSITION:
-                break;
-            case SC_MOTOR_SPEED:
-                break;
-            /* default: */
-            /*     //request error: invalid command */
-
-        }
-
-
-      /* //setting new servo target */
-      /* if(serial_values[SERVO_SERIAL_FIELD] >= 0 and serial_values[SERVO_SERIAL_FIELD] <= 1024) */
-      /* { */
-      /*   servoPosTarget = serial_values[SERVO_SERIAL_FIELD]; */
-      /*   Serial.print("OK,"); */
-      /* } */
-      /* else */
-      /* { */
-      /*   // invalid servo target value */
-      /*   Serial.print("E2,"); */
-      /* } */
-
-      /* if(serial_values[MOTOR_SERIAL_FIELD] >= MOTOR_VALUE_MIN && serial_values[MOTOR_SERIAL_FIELD] <= MOTOR_VALUE_MAX) */
-      /* { */
-      /*   setMotorSpeed(serial_values[MOTOR_SERIAL_FIELD]); */
-      /*   Serial.print("OK"); */
-      /* } */
-      /* else */
-      /* { */
-      /*   // invalid motor speed target value */
-      /*   Serial.print("E3"); */
-      /* } */
-      /* Serial.println(""); */
-
-
-      /* for(int i=0; i < min(SERIAL_FIELDS_COUNT, fieldIndex+1); i++) */
-      /* { */
-      /*   serial_values[i] = 0; // set the values to zero, ready for the next message */
-      /* } */
-      /* fieldIndex = 0;  // ready to start over */
-    }
-    else
-    {
-        //error
-    }
-  }
+    status = processSerial();
+    
+    status = possitionServo();
 }
-
